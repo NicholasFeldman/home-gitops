282d0bdcd87cf969f92bc0dda6c30b313e3075b0
Nicholas Feldman <nick@feldman.tech>
2024-03-25 12:24:52 -0400

    Use Authentik from Argo

---
+++ terraform/authentik/argocd.tf
@@ -0,0 +1,26 @@
+resource "authentik_provider_oauth2" "argocd-oauth2" {
+    name = "ArgoCD - Oauth2"
+    client_id = "argocd"
+    authorization_flow = data.authentik_flow.default-provier-authorization-implicit-consent.id
+
+    redirect_uris = [ "https://argocd.feldman.in/auth/callback" ]
+    property_mappings = [
+        data.authentik_scope_mapping.scope-email.id,
+        data.authentik_scope_mapping.scope-profile.id,
+        data.authentik_scope_mapping.scope-openid.id,
+    ]
+}
+
+resource "authentik_application" "argocd" {
+    name = "ArgoCD - Oauth2"
+    slug = "argocd"
+    protocol_provider = authentik_provider_oauth2.argocd-oauth2.id
+}
+
+resource "authentik_group" "argocd_admins" {
+  name    = "ArgoCD Admins"
+}
+
+resource "authentik_group" "argocd_viewers" {
+  name    = "ArgoCD Viewers"
+}
