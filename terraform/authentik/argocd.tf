b867a10ae95462b8c5882fea4fa13d4bdc52508f
Nicholas Feldman <nick@feldman.tech>
2024-03-25 14:05:08 -0400

    Use dex config for argo

--- terraform/authentik/argocd.tf
+++ terraform/authentik/argocd.tf
@@ -3,7 +3,10 @@ resource "authentik_provider_oauth2" "argocd-oauth2" {
     client_id = "argocd"
     authorization_flow = data.authentik_flow.default-provier-authorization-implicit-consent.id
 
-    redirect_uris = [ "https://argocd.feldman.in/auth/callback" ]
+    redirect_uris = [
+      "https://argocd.feldman.in/auth/callback",
+      "https://argocd.feldman.in/api/dex/callback"
+      ]
     property_mappings = [
         data.authentik_scope_mapping.scope-email.id,
         data.authentik_scope_mapping.scope-profile.id,
